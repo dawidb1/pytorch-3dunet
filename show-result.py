5e125e104cae909305eb9c7a6984eb976c3d059a
dawidb1 <dawid.baranski.96@gmail.com>
2020-01-05 14:34:20 +0100

    add h5 reader

---
+++ show-result.py
@@ -0,0 +1,13 @@
+import h5py
+import numpy as np
+
+filename = "resources\\random_label3D_predictions.h5"
+
+with h5py.File(filename, 'r') as f:
+    # List all groups
+    print("Keys: %s" % f.keys())
+    a_group_key = list(f.keys())[0]
+
+    # Get the data
+    data = list(f[a_group_key])
+    print(data)
